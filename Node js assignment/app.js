ba664f8a21d0adc7ca42529419825252374fc25d
Aryan Gupta <aryan.gupta3_cs23@gla.ac.in>
2026-01-15 18:05:57 +0530

    [Aryan] Add: Solution of custom module

---
+++ Node js assignment/app.js
@@ -0,0 +1,7 @@
+const stringUtils = require('./stringUtils')
+
+const message = 'node js module'
+
+console.log(stringUtils.capitalize(message))
+console.log(stringUtils.reverseString(message))
+console.log(stringUtils.countVowels(message))
