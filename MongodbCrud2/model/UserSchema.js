aa77527c99a2244572046073993661e88538f974
Aryan Gupta <aryan.gupta3_cs23@gla.ac.in>
2026-03-26 09:44:04 +0530

    [Aryan]: update userScheam.js

--- MongodbCrud2/model/UserSchema.js
+++ MongodbCrud2/model/UserSchema.js
@@ -24,5 +24,13 @@ const userSchema  = new mongoose.Schema({
     }
 })
 
+userSchema.pre("save", async function(next){
+    if(!this.isModified("password")){
+        return ;
+    }
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+   
+})
 const user = mongoose.model("user", userSchema)
 export default user;
